683f0546663d1aaab5a47dccbaa09d92407c2255
Mathias Bosman <bosmanmathias@gmail.com>
2019-11-08 10:37:11 +0100

    Removed the warning for spring.jpa.open-in-view

--- vim-web/src/main/java/be/vizit/vim/app/VimWebApp.java
+++ vim-web/src/main/java/be/vizit/vim/app/VimWebApp.java
@@ -1,12 +1,17 @@
 package be.vizit.vim.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication //(scanBasePackages = "be.vizit")
 public class VimWebApp {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VimWebApp.class);
+
 	public static void main(String[] args) {
         SpringApplication.run(VimWebApp.class, args);
-        System.out.println("VIM started");
+        LOGGER.info("VIM web application has started");
 	}
 }

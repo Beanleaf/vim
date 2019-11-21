c71d278f015962f7a0e96f7fe38ceb23583fd993
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2019-11-21 17:57:16 +0100

    Referenced columns need their column names attached

--- vim-web/src/main/resources/db/migration/versioned/V3__items_schema.sql
+++ vim-web/src/main/resources/db/migration/versioned/V3__items_schema.sql
@@ -1,12 +1,12 @@
 create table inventory_items
 (
-    id            serial                not null
+    id               serial                not null
         constraint inventory_items_pk
             primary key,
-    active        boolean default false not null,
-    uuid          varchar(36),
-    description   varchar(255),
-    item_category serial
+    active           boolean default false not null,
+    uuid             varchar(36),
+    description      varchar(255),
+    item_category_id int
 );
 
 create unique index inventory_items_id_uindex
@@ -17,4 +17,4 @@ create unique index inventory_items_uuid_uindex
 
 alter table inventory_items
     add constraint inventory_items_item_categories_id_fk
-        foreign key (item_category) references item_categories;
+        foreign key (item_category_id) references item_categories;
--- vim-web/src/main/resources/db/migration/versioned/V4__inventory_logs.sql
+++ vim-web/src/main/resources/db/migration/versioned/V4__inventory_logs.sql
@@ -3,10 +3,10 @@ create table inventory_logs
     id                  serial      not null
         constraint inventory_logs_pk
             primary key,
-    inventory_item      serial      not null,
-    "user"              serial      not null,
+    inventory_item_id   int         not null,
+    user_id             int         not null,
     timestamp           timestamptz not null,
-    inventory_direction serial      not null,
+    inventory_direction int         not null,
     comment             varchar(255)
 );
 
@@ -15,8 +15,8 @@ create unique index inventory_logs_id_uindex
 
 alter table inventory_logs
     add constraint inventory_logs_inventory_items_id_fk
-        foreign key (inventory_item) references inventory_items;
+        foreign key (inventory_item_id) references inventory_items;
 
 alter table inventory_logs
     add constraint inventory_logs_users_id_fk
-        foreign key ("user") references users;
+        foreign key (user_id) references users;

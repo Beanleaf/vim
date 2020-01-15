c68dba1f4bc499026a4320c6db3a6cbd738b70e0
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2020-01-15 10:37:13 +0100

    [#29] Added language switcher when not yet logged in

--- vim-web/src/main/java/be/beanleaf/vim/app/VimLocaleResolver.java
+++ vim-web/src/main/java/be/beanleaf/vim/app/VimLocaleResolver.java
@@ -5,11 +5,15 @@ import be.beanleaf.vim.domain.entities.User;
 import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class VimLocaleResolver extends SessionLocaleResolver {
+public class VimLocaleResolver extends SessionLocaleResolver implements WebMvcConfigurer {
 
   @Autowired
   private VimSession vimSession;
@@ -22,5 +26,17 @@ public class VimLocaleResolver extends SessionLocaleResolver {
       return userLocale != null ? userLocale : super.resolveLocale(request);
     }
     return super.resolveLocale(request);
+  }
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
   }
 }

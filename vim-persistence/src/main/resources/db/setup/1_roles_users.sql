947f6ba7363d63b2f6c6aca0992b5893582f57b1
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2020-06-16 11:00:03 +0200

    Added afterMigrate.sql for postgres grants

--- vim-persistence/src/main/resources/db/setup/1_roles_users.sql
+++ vim-persistence/src/main/resources/db/setup/1_roles_users.sql
@@ -27,21 +27,21 @@ $$
     END
 $$;
 
-/* Create dba and application user */
 DO
 $$
     BEGIN
         IF NOT EXISTS(
                 SELECT *
-                FROM pg_catalog.pg_roles
-                WHERE rolname = 'vim_dba'
+                FROM pg_catalog.pg_group
+                WHERE groname = 'vim_ro'
             )
         THEN
-            EXECUTE 'CREATE ROLE vim_dba LOGIN PASSWORD ''vim_dba'' VALID UNTIL ''infinity'';';
+            EXECUTE 'CREATE ROLE vim_ro;';
         END IF;
     END
 $$;
 
+/** Create application user and flyway application user with default passwords */
 DO
 $$
     BEGIN
@@ -62,10 +62,10 @@ $$
         IF NOT EXISTS(
                 SELECT *
                 FROM pg_catalog.pg_roles
-                WHERE rolname = 'vim_ro'
+                WHERE rolname = 'vim_dba'
             )
         THEN
-            EXECUTE 'CREATE ROLE vim_ro LOGIN PASSWORD ''vim_ro'' VALID UNTIL ''infinity'';';
+            EXECUTE 'CREATE ROLE vim_dba LOGIN PASSWORD ''vim_dba'' VALID UNTIL ''infinity'';';
         END IF;
     END
 $$;
@@ -76,15 +76,18 @@ $$
     BEGIN
         EXECUTE 'GRANT CONNECT ON DATABASE ' || current_database() ||
                 ' TO vim_ddl, vim_dml, vim_ro;';
-        EXECUTE 'GRANT ALL ON DATABASE ' || current_database() || ' TO vim_dba;';
+        EXECUTE 'GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public' ||
+                ' TO vim_dml;';
+        EXECUTE 'GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO vim_ddl;';
+        EXECUTE 'GRANT ALL ON DATABASE ' || current_database() || ' TO vim_ddl;';
         EXECUTE 'REVOKE ALL ON DATABASE ' || current_database() || ' FROM public;';
-    END;
+    END
 $$;
 
 /* Grant our application user and flyway user the correct role and privileges to get started */
+GRANT USAGE ON SCHEMA PUBLIC TO vim_dml;
+GRANT USAGE ON SCHEMA PUBLIC TO vim_ro;
 GRANT vim_dml TO vim;
 GRANT vim_ddl TO vim_dba;
 REVOKE ALL ON SCHEMA PUBLIC FROM PUBLIC;
 GRANT ALL ON SCHEMA PUBLIC TO vim_ddl;
-GRANT USAGE ON SCHEMA PUBLIC TO vim_dml;
-GRANT USAGE ON SCHEMA PUBLIC TO vim_ro;

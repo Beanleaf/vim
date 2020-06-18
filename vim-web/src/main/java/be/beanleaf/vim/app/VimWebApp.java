972509fc48ddb37c3eecf8c0e75eed7914bd8b1d
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2020-06-18 08:32:13 +0200

    Changed validations location

--- vim-web/src/main/java/be/beanleaf/vim/app/VimWebApp.java
+++ vim-web/src/main/java/be/beanleaf/vim/app/VimWebApp.java
@@ -24,13 +24,15 @@ public class VimWebApp extends SpringBootServletInitializer {
   @Value("${spring.messages.encoding:UTF-8}")
   private String encoding;
 
+  private static final String messagePath = "classpath:/messages/";
+
   @Bean
   public MessageSource messageSource() {
     ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
     messageSource.setBasenames(
-        "classpath:/messages/messages",
-        "classpath:/messages/messages.mail",
-        "classpath:/messages/messages.errors");
+        messagePath + "messages",
+        messagePath + "messages.mail",
+        messagePath + "messages.errors");
     messageSource.setDefaultEncoding(encoding);
     return messageSource;
   }
@@ -39,7 +41,7 @@ public class VimWebApp extends SpringBootServletInitializer {
   public LocalValidatorFactoryBean validator() {
     LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
     ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-    messageSource.setBasename("classpath:validations");
+    messageSource.setBasename(messagePath + "validations");
     messageSource.setDefaultEncoding(encoding);
     bean.setValidationMessageSource(messageSource);
     return bean;
